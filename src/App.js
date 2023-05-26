4bcc845262d6e9809c5da0a3a1a1999d142c91be
AndresBo <anndresfborrero@gmail.com>
2023-05-26 21:17:37 +1000

    moved API KEY to a const variable

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ function App() {
   const [query, setQuery] = useState("");
 
   const API_URL = "https://api.api-ninjas.com/v1/historicalevents?text=";
+  const API_KEY = "SymaCCHQRJ8PQzoD9RpclA==BLj9oqEyCrR6EkD5";
 
   const handleInputChange = (e) => {
     setQuery(e.target.value);
   }
 
   const fetchEventData = () => {
-    fetch(API_URL + query, {headers: {"X-Api-Key": "SymaCCHQRJ8PQzoD9RpclA==BLj9oqEyCrR6EkD5"}})
+    fetch(API_URL + query, {headers: {"X-Api-Key": API_KEY}})
     .then(response => {return response.json()})
     .then(data => {setEvent(data)})
   }
